281d882d4375a9fb3f42c7299f964652c63eafcf
jnudqcg6 <karadshf@gmail.com>
2019-01-09 13:03:16 -0500

    First HW assignment

---
+++ helloWorld/main.cpp
@@ -0,0 +1,14 @@
+/*
+ *  Farid Karadsheh
+ *  GVSU CIS 263-02
+ *  1/9/2019
+ *  Professor Xiang Cao
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, World!" << endl;
+    return 1;
+}
